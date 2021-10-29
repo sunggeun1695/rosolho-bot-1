e8665db952e2f76789671977ae2b8cb175fb16e8
Rosolho <73169541+sunggeun1695@users.noreply.github.com>
2021-10-29 09:33:24 +0900

    Update client.js

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 const config = {
-    token: "ODgwMjI4ODM3OTg2ODExOTI0.YSbO_A.7s-IBzThoi0Ofn2uBd7KLb1FFhE",
+    token: "TOKEN",
     prefix: "!!",
-    mongo: "mongodb+srv://rosolhome:l1054821@rosolhobot.ja9up.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    mongo: "MONGO URI"
 };
 
 module.exports = config;
